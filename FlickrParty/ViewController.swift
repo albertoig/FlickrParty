550d23e2fed00d5d2b09f6d32d34f721c3af161e
Alberto Iglesias Gallego <alberto.uchihagmail.com>
2015-03-24 20:31:44 +0100

    Initial Commit

---
+++ FlickrParty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrParty
+//
+//  Created by Alberto Iglesias Gallego on 24/3/15.
+//  Copyright (c) 2015 albertchurch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
